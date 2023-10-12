925908659cccec8223711125733580835e4c0e0b
meganyim <meganyim@andrew.cmu.edu>
2023-10-11 23:20:32 -0400

    done with hw5

--- solution-hw5/js/app.js
+++ solution-hw5/js/app.js
@@ -59,6 +59,24 @@ addNewRoll("Apple", "Original", 3, 3.49)
 
 // console.log(cart)
 
+
+// creating delete function
+function deleteRoll(roll) {
+    document.getElementById(roll.type).remove();
+    // getting rid of it from the array
+    let i = 0;
+    for (let roll of cart) {
+        if (cart[i].type==roll.type) {
+            cart.splice(i,1);
+        }
+        else {
+            i++;
+        }
+    }
+    // totalPrice(cart);
+    const sumPrice = document.querySelector("#cartPrice");
+    sumPrice.innerText = "$" + (totalPrice(cart)).toFixed(2);
+}
 // add to DOM
 
 function displayRoll(roll) {
@@ -66,48 +84,76 @@ function displayRoll(roll) {
     let rollElement = document.createElement("div");
     //assigning an id to all the roll Element divs based on the name of the roll type
     rollElement.id = roll.type;
+    rollElement.classList.add("roll-container");
+
+    // create a second div for remove button
+    let mainContainer = document.createElement("div");
+    mainContainer.classList.add("main-container");
+
+    // create a div to make text stay in columns
+    let textContainer = document.createElement("div");
     
     // get the image for each product item
     let item_img = document.createElement("img");
+    item_img.classList.add("item-img");
     item_img.src = '../assets/products/'+roll.type+"-cinnamon-roll.jpg";
-    rollElement.appendChild(item_img);
+    mainContainer.appendChild(item_img);
     
+    mainContainer.appendChild(textContainer);
+
     //get the item name for each product item
     let item_name = document.createElement("div");
-    item_name.innerText = roll.type + "Cinnamon Roll";
-    rollElement.appendChild(item_name);
+    item_name.classList.add("item-name");
+    item_name.innerText = roll.type + " Cinnamon Roll";
+    textContainer.appendChild(item_name);
 
     //get the glaze for each product Item
     let item_glaze = document.createElement("div");
+    item_glaze.classList.add("item-glaze");
     item_glaze.innerText = roll.glazing;
-    rollElement.appendChild(item_glaze);
+    textContainer.appendChild(item_glaze);
 
     //get the pack size for each product item
     let item_pack = document.createElement("div");
-    item_pack,innerText = "Pack Size: " + roll.size;
-    rollElement.appendChild(item_pack);
+    item_pack.classList.add("item-pack");
+    item_pack.innerText = "Pack Size: " + roll.size;
+    textContainer.appendChild(item_pack);
 
     //get calculated price for each product item
     let item_price = document.createElement("div");
-    item_price.innerText = string(roll.calculatedPrice)
-    rollElement.appendChild(item_price);
+    item_price.classList.add ("item-price");
+    item_price.innerText = "$"+String(roll.calculatedPrice);
+    mainContainer.appendChild(item_price);
 
-    //remove button
-    let removeButton = document.createElement("button");
-    removeButton.onClick = () => {
-        document.getElementById(roll.type).remove();
-        for (const i in cart) {
-            if (cart[i].type === roll.type) {
-                delete cart[i];
-            }
+    //making the button
+    let removeButton = document.createElement("p");
+    removeButton.classList.add("rem-btn")
+    removeButton.innerText = "Remove";
+
+    // making remove button functionality
+    removeButton.addEventListener("click", () => {
+        deleteRoll(roll);
         }
-        updatePrice();
-    }
+    )
+
+    rollElement.appendChild(mainContainer);
     rollElement.appendChild(removeButton);
 
     shopping_cart.appendChild(rollElement);
 }
 
-cart.forEach(displayRoll);
+// create the function for the cart total price
+function totalPrice(cart) {
+    let sum = 0;
+    for (const roll of cart) {
+        const num = parseFloat(roll.calculatedPrice);
+        sum = sum + num;
+    }
+    return(sum);
+}
 
-console.log(displayRoll)
+// display the cart total price
+const sumPrice = document.querySelector("#cartPrice");
+sumPrice.innerText = "$" + totalPrice(cart);
+
+cart.forEach(displayRoll);
