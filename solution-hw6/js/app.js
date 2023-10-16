75c5ddfa7f0f9b927605bd07369d74b9fddce2a5
meganyim <meganyim@andrew.cmu.edu>
2023-10-16 12:58:03 -0700

    hw6

---
+++ solution-hw6/js/app.js
@@ -0,0 +1,104 @@
+// initialzing "Cart"
+let cart = [];
+
+//save data to local storage
+function saveToLocalStorage () {
+    localStorage.setItem("storedCart", JSON.stringify(cart));
+    console.log(storedCart);
+};
+
+//retrieving data from the local storage
+const storedCart = localStorage.getItem("storedCart")
+    if (storedCart !== null) {
+        cart = JSON.parse(storedCart);
+}
+
+// add to DOM
+
+function displayRoll(roll) {
+    let shopping_cart = document.getElementById("cart-list");
+    let rollElement = document.createElement("div");
+    //assigning an id to all the roll Element divs based on the name of the roll type
+    rollElement.id = roll.type;
+    rollElement.classList.add("roll-container");
+
+    // create a second div for remove button
+    let mainContainer = document.createElement("div");
+    mainContainer.classList.add("main-container");
+
+    // create a div to make text stay in columns
+    let textContainer = document.createElement("div");
+    textContainer.classList.add("text-container");
+    
+    // get the image for each product item
+    let item_img = document.createElement("img");
+    item_img.classList.add("item-img");
+    item_img.src = '../assets/products/'+roll.type+"-cinnamon-roll.jpg";
+    mainContainer.appendChild(item_img);
+    
+    mainContainer.appendChild(textContainer);
+
+    //get the item name for each product item
+    let item_name = document.createElement("div");
+    item_name.classList.add("item-name");
+    item_name.innerText = roll.type + " Cinnamon Roll";
+    textContainer.appendChild(item_name);
+
+    //get the glaze for each product Item
+    let item_glaze = document.createElement("div");
+    item_glaze.classList.add("item-glaze");
+    item_glaze.innerText = roll.glazing;
+    textContainer.appendChild(item_glaze);
+
+    //get the pack size for each product item
+    let item_pack = document.createElement("div");
+    item_pack.classList.add("item-pack");
+    item_pack.innerText = "Pack Size: " + roll.size;
+    textContainer.appendChild(item_pack);
+
+    //get calculated price for each product item
+    let item_price = document.createElement("div");
+    item_price.classList.add ("item-price");
+    item_price.innerText = "$"+String(roll.calculatedPrice);
+    mainContainer.appendChild(item_price);
+
+    //making the button
+    let removeButton = document.createElement("p");
+    removeButton.classList.add("rem-btn")
+    removeButton.innerText = "Remove";
+
+    // making remove button functionality
+    //.parentNode pulls the parent element of the removeButton, in this case the rollContainer
+    removeButton.addEventListener("click", () => {
+        removeButton.parentNode.remove();
+        //creates a new array that passes the test of the function that is passed into .filter
+        cart = cart.filter((item) => item !== roll);
+
+        const sumPrice = document.querySelector("#cartPrice");
+        sumPrice.innerText = "$" + (totalPrice(cart)).toFixed(2);
+
+        saveToLocalStorage();
+        }
+    )
+
+    rollElement.appendChild(mainContainer);
+    rollElement.appendChild(removeButton);
+
+    shopping_cart.appendChild(rollElement);
+}
+
+// create the function for the cart total price
+function totalPrice(cart) {
+    let sum = 0;
+    for (const roll of cart) {
+        const num = parseFloat(roll.calculatedPrice);
+        sum = sum + num;
+    }
+    return(sum);
+}
+
+// display the cart total price
+const sumPrice = document.querySelector("#cartPrice");
+sumPrice.innerText = "$" + (totalPrice(cart)).toFixed(2);
+
+cart.forEach(displayRoll);
---
+++ solution-hw6/js/rollsData.js
@@ -0,0 +1,124 @@
+const rolls = {
+    "Original": {
+        "basePrice": 2.49,
+        "imageFile": "original-cinnamon-roll.jpg"
+    },
+    "Apple": {
+        "basePrice": 3.49,
+        "imageFile": "apple-cinnamon-roll.jpg"
+    },
+    "Raisin": {
+        "basePrice": 2.99,
+        "imageFile": "raisin-cinnamon-roll.jpg"
+    },
+    "Walnut": {
+        "basePrice": 3.49,
+        "imageFile": "walnut-cinnamon-roll.jpg"
+    },
+    "Double-Chocolate": {
+        "basePrice": 3.99,
+        "imageFile": "double-chocolate-cinnamon-roll.jpg"
+    },
+    "Strawberry": {
+        "basePrice": 3.99,
+        "imageFile": "strawberry-cinnamon-roll.jpg"
+    }    
+};
+
+class Roll {
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+    }
+}
+
+const queryString = window.location.search;
+console.log(queryString)
+
+const params = new URLSearchParams(queryString);
+console.log(params);
+
+const rollType = params.get('roll')
+console.log(rollType);
+
+// Update the header text
+const headerElement = document.querySelector("#chosenBun-header");
+headerElement.innerText = rollType + " Cinnamon Roll";
+
+//Update Image
+const productImage = document.querySelector('#product-img');
+productImage.src = '../assets/products/' + rolls[rollType].imageFile;
+
+let cart = [];
+
+function cartFunction() {
+
+    const chooseGlaze = document.querySelector("#glazing-dropdown");
+    const rollGlazing = chooseGlaze.options[chooseGlaze.selectedIndex].text;
+    const choosePack = document.querySelector("#pack-size");
+    const packSize = choosePack.options[choosePack.selectedIndex].text;
+    const basePrice = rolls[rollType].basePrice
+    let addRoll = new Roll(rollType, rollGlazing, packSize, basePrice);
+
+    
+    let glazePrice = 0
+    // creating the glazePrice based on rollGlazing 
+    if (rollGlazing === "Original") {
+            glazePrice = 0
+        }
+        else if (rollGlazing === "Sugar Milk") {
+            glazePrice = 0
+        }
+        else if (rollGlazing === "Vanilla Milk") {
+            glazePrice = 0.50
+        }
+        else {
+            glazePrice = 1.50
+        }
+    
+
+    //  multiplying the right amount for packSize
+    let packPrice = 0
+    if (packSize === "1") {
+            packPrice = 1
+        }
+        else if (packSize === "3") {
+            packPrice = 3
+        }
+        else if (packSize === "6") {
+            packPrice = 5
+        }
+        else {
+            packPrice = 10
+        }
+
+    addRoll.calculatedPrice =  ((basePrice + glazePrice) * packPrice).toFixed(2);
+    cart.push(addRoll);
+    console.log(cart);
+
+    saveToLocalStorage();
+}
+
+function saveToLocalStorage () {
+    localStorage.setItem("storedCart", JSON.stringify(cart));
+    console.log(storedCart);
+};
+
+//retrieving data from the local storage
+let cartFromStorage = [];
+const storedCart = localStorage.getItem("storedCart")
+    if (storedCart !== null) {
+        cart = JSON.parse(storedCart);
+}
+
+// function retrieveFromLocalStorage() {
+//     const getStoredCart = localStorage.getItem('storedCart');
+//     const storedCartArray = JSON.parse(getStoredCart);
+//     for (const item of storedCartArray) {
+//       const cartItem = addNewRoll(item.rollType, item.rollGlazing, item.packSize, item.rollPrice);
+//       createElement(cartItem);
+//     }
+//   }
+  
